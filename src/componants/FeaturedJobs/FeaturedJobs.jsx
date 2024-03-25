5be323fd867c4014323f2cb753e461b4de382007
Alamin <mdalaminhossain573190@gmail.com>
2024-03-25 17:42:34 +0600

    3th commit

--- src/componants/FeaturedJobs/FeaturedJobs.jsx
+++ src/componants/FeaturedJobs/FeaturedJobs.jsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import Job from "../Job/Job";
 
 
+
 const FeaturedJobs = () => {
     const [jobs , setJobs] = useState([]);
 
     useEffect(() =>{
         fetch('/public/data/jobs.json')
     .then(res => res.json())
-    .then(data => setJobs(data.data));
+    .then(data => setJobs(data))
+   
 },[]);
     return (
         <div>
@@ -16,13 +18,14 @@ const FeaturedJobs = () => {
             <h2 className="text-5xl font-bold text-center mb-5">Featured Jobs:{jobs.length}</h2>
             <p className="text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
         </div>
-        <div>
-            {
-                jobs.map(job => <Job key={jops.id} job={job}></Job>)
-            }
-        </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                {
+                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                }
+            </div>
         </div>
     );
 };
+
 
 export default FeaturedJobs;
