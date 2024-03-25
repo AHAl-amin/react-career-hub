6a0146e125e655480889b55c357f4e42018c7b57
Alamin <mdalaminhossain573190@gmail.com>
2024-03-25 20:27:07 +0600

    4th commit

--- src/componants/Job/Job.jsx
+++ src/componants/Job/Job.jsx
@@ -1,5 +1,5 @@
-
-
+import { CiLocationOn } from "react-icons/ci";
+import { CiDollar } from "react-icons/ci";
 const Job = ({job}) => {
     const {logo,job_title,company_name,remote_or_onsite, location, job_type, salary} = job;
     return (
@@ -12,8 +12,12 @@ const Job = ({job}) => {
             <button className="px-5 py-2 font-extrabold border rounded border-blue-500">{remote_or_onsite}</button>
             <button  className="px-5 py-2 font-extrabold border rounded border-blue-500"> {job_type}</button>
           </div>
+        <div className="flex items-center gap-6">
+        <h2 className="flex gap-3 items-center"><CiLocationOn />{location}</h2>
+        <h2 className="flex gap-3 items-center"> <CiDollar /> salery:{salary}</h2>
+        </div>
           <div className="card-actions justify-start">
-            <button className="btn btn-primary">View Details</button>
+            <button className="btn bg-gradient-to-r from-cyan-500 to-blue-500">View Details</button>
           </div>
         </div>
       </div>
